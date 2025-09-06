21ba130260a52b96412657d52559cefa7fed56fb
amira-96 <amirahasan853@gmail.com>
2025-09-05 23:57:29 -0700

    modify in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import { HashRouter as Router } from 'react-router-dom';
 
 import viteLogo from '/vite.svg'
 import './App.css'
